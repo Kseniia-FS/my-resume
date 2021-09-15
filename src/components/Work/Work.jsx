b6a931a0b8c3fdfa7ca4600b5ba0826337997f01
Kseniia-FS <snabukraine@ukr.net>
2021-09-15 11:41:56 +0300

    Add work

--- src/components/Work/Work.jsx
+++ src/components/Work/Work.jsx
@@ -1,5 +1,8 @@
 import s from "./Work.module.css";
 import WorkIcon from "../../images/work.svg";
+import Bergge from "../../images/logo1.png";
+import Farbaland from "../../images/logo.png";
+import Vivat from "../../images/logo-vivat.png";
 const Work = () => {
   return (
     <section className={s.work}>
@@ -13,22 +16,40 @@ const Work = () => {
           <p className={s.name}>CEO</p>
           <ul>
             <li>
-              Throughout the entire work, I achieved an increase in the overall
-              conversion up to 63%, more stores were opened in Kiev, staff
-              turnover disappeared, some of the employees achieved personal
-              career heights. Also I opened a new direction of b2b sales for the
-              company.
+              During the whole work there were many achievements that I am proud
+              of. I have implemented three large projects from scratch, managed
+              the team at all stages of implementation. The main challenge I
+              successfully completed was to create working brands that the
+              consumer can trust. Starting from logo design, brand book
+              creation, to website development and conversion landing pages. And
+              most important, the creation of a sales department for each
+              project. I am proud to invite you to familiarize yourself with the
+              projects.
             </li>
           </ul>
         </div>
         <div className={s.wrapAbout}>
-          <p className={s.aboutWork}>2008-2010</p>
-          <p className={s.name}>Sales manager</p>
-          <ul>
-            <li>
-              During the work, I achieved the conclusion of an agreement with
-              large companies, was a tutor on sales trainings, and have
-              maintained warm relations with key customers until now.
+          <p className={s.aboutWork}>projects:</p>
+
+          <ul className={s.brands}>
+            <li className={s.bergge}>
+              <a href="https://bergge.com/" target="_blank" rel="noreferrer">
+                <img src={Bergge} alt="" />
+              </a>
+            </li>
+            <li className={s.bergge}>
+              <a href="https://farbaland.com/" target="_blank" rel="noreferrer">
+                <img src={Farbaland} alt="" />
+              </a>
+            </li>
+            <li className={`${s.bergge} ${s.vivat}`}>
+              <a
+                href="https://vivatdecor.com/"
+                target="_blank"
+                rel="noreferrer"
+              >
+                <img src={Vivat} alt="" />
+              </a>
             </li>
           </ul>
         </div>
